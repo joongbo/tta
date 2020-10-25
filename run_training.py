e866833a87d91f85d038911647f4e64fb4e312c0
joongbo <joongbo32@gmail.com>
2020-10-26 03:04:29 +0900

    Copyrights

--- run_training.py
+++ run_training.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Run masked LM pre-training for BERT."""
+"""Run Language Autoencoding pre-training for TTA."""
 
 from __future__ import absolute_import
 from __future__ import division
